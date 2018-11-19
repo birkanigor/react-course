2be86318a0b46a797f21c3668c94f75b3953e08b
igorbirkan <igor.birkan@gmail.com>
2018-11-19 21:04:45 +0200

    lesson-18

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state={
@@ -9,12 +10,22 @@ class App extends Component {
       {name : 'Yaniv' , age: 13, belt :'green' , id : 3}
     ]
   }
+
+  addNinja=(ninja)=>{
+    ninja.id = Math.random();
+    let newNinjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas:newNinjas
+    })
+  }
+
   render() {
     return (
       <div className="App">
           <h1>My firt react app !</h1>
           <p>Welcome :)</p>
           <Ninjas ninjas={this.state.ninjas}/>
+          <AddNinja addNinja={this.addNinja}></AddNinja>
       </div>
     );
   }
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Ninjas =({ ninjas }) => {    
     const ninjaList = ninjas.map(ninja => {
-        return ninja.age>20 ?(
+        return ninja.age>2 ?(
             <div className="ninja" key={ninja.id}>
                 <div><b>Name : {ninja.name}</b></div>
                 <div>Age : {ninja.age}</div>
